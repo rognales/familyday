a996843f4a6f3ca553543a8006ed143e555a0c22
Zaiman Noris <rognales@gmail.com>
2020-03-06 01:18:06 +0800

    Enable daily pruning for Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new FlaggingMembersJob)->daily();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
