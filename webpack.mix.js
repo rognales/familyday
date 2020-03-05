14309532410ae3cb8d5c8c97a46fc6160a549dbc
Zaiman Noris <rognales@gmail.com>
2020-03-06 02:32:08 +0800

    Laravel missed out default files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +10,6 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.extract(['vue', 'jquery']);
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
