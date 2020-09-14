e3abdb8a96adfa91139442455bc8f1823b5a86d9
Zaiman Noris <rognales@gmail.com>
2020-09-15 02:46:01 +0800

    Refresh ide-helpers

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -20,33 +20,31 @@ namespace App{
  * @property string|null $staff_id
  * @property string|null $age
  * @property int $participant_id
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read \App\Participant $member
  * @property-read \App\Participant $participant
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant adult()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant family()
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant infant()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant kids()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant newQuery()
- * @method static \Illuminate\Database\Query\Builder|\App\Dependant onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant others()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant query()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereAge($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereParticipantId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereRelationship($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereStaffId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Dependant whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Dependant withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Dependant withoutTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant adult()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant family()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant infant()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant kids()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Dependant onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant others()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereAge($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereParticipantId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereRelationship($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereStaffId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Dependant whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Dependant withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Dependant withoutTrashed()
  */
 	class Dependant extends \Eloquent {}
 }
@@ -60,14 +58,14 @@ namespace App{
  * @property string $staff_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member whereStaffId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Member whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Member newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Member newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Member query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Member whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Member whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Member whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Member whereStaffId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Member whereUpdatedAt($value)
  */
 	class Member extends \Eloquent {}
 }
@@ -76,9 +74,9 @@ namespace App{
 /**
  * App\Model
  *
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Model query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Model newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Model newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Model query()
  */
 	class Model extends \Eloquent {}
 }
@@ -100,10 +98,10 @@ namespace App{
  * @property \App\User $attend
  * @property string|null $attend_timestamp
  * @property int|null $attended_by
+ * @property int|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property int|null $deleted_by
  * @property int $is_vege
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Dependant[] $adults
  * @property-read int|null $adults_count
@@ -127,33 +125,31 @@ namespace App{
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Dependant[] $othersKids
  * @property-read int|null $others_kids_count
  * @property-read \App\User $payment
- * @property-read \App\User $softDeletedBy
- * @method static bool|null forceDelete()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant newQuery()
- * @method static \Illuminate\Database\Query\Builder|\App\Participant onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant query()
- * @method static bool|null restore()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereAttend($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereAttendTimestamp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereAttendedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereDivision($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereIsVege($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereMember($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant wherePaymentBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant wherePaymentDetails($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant wherePaymentStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant wherePaymentTimestamp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereStaffId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Participant whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Participant withTrashed()
- * @method static \Illuminate\Database\Query\Builder|\App\Participant withoutTrashed()
+ * @property-read \App\User|null $softDeletedBy
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Participant onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereAttend($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereAttendTimestamp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereAttendedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereDivision($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereIsVege($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereMember($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant wherePaymentBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant wherePaymentDetails($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant wherePaymentStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant wherePaymentTimestamp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereStaffId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Participant whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Participant withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Participant withoutTrashed()
  */
 	class Participant extends \Eloquent {}
 }
@@ -167,14 +163,14 @@ namespace App{
  * @property string $staff_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff whereStaffId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Staff whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereStaffId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereUpdatedAt($value)
  */
 	class Staff extends \Eloquent {}
 }
@@ -187,6 +183,7 @@ namespace App{
  * @property string $username
  * @property string $name
  * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
  * @property string $password
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
@@ -194,18 +191,19 @@ namespace App{
  * @property int $active
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereActive($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUsername($value)
  */
 	class User extends \Eloquent {}
 }
