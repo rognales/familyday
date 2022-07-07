31ff37a976a06d784c50bc11de98e1be9506d42d
Zaiman Noris <rognales@gmail.com>
2022-07-07 11:00:42 +0800

    removed laravel/telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new FlaggingMembersJob)->daily();
-        $schedule->command('telescope:prune')->daily();
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment() == 'local') {
-            $this->app->register(TelescopeServiceProvider::class);
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
     }
