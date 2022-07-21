8a1abab36e3491620aca798e932843f28f1816f9
Zaiman Noris <rognales@gmail.com>
2022-07-22 00:35:41 +0800

    chore: remove livechat

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -515,21 +515,6 @@
 
         gtag('config', 'UA-1014386-11');
     </script>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/5e9ff0f969e9320caac61942/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
 </body>
 
 </html>
