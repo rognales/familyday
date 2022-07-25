961e11e1eb6b7c5a3d691d319ba525a37c330b59
Zaiman Noris <rognales@gmail.com>
2022-07-26 00:06:08 +0800

    chore: update seeder to 1000

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,23 +36,24 @@ class DatabaseSeeder extends Seeder
 
         // Only seed test data in non-prod
         if (! app()->isProduction()) {
-            Member::factory()->count(10)->create();
-            Staff::factory()->count(10)->create();
+            Member::factory()->count(1000)->create();
+            Staff::factory()->count(1000)->create();
 
             Participant::factory()
                 ->addSpouse()
                 ->addChildren()
                 ->addInfant()
                 ->addOthers()
-                ->count(15)
+                ->count(500)
+                ->hasUploads(2)
                 ->create();
 
-            Upload::factory()
-            ->count(5)
-            ->state(new Sequence(
-                fn ($sequence) => ['participant_id' => Participant::all()->random()],
-            ))
-            ->create();
+            // Upload::factory()
+            // ->count(5)
+            // ->state(new Sequence(
+            //     fn ($sequence) => ['participant_id' => Participant::all()->random()],
+            // ))
+            // ->create();
         }
     }
 }
