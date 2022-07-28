eabf4feacf406989f9b6ce8ec8f43d3305149212
Zaiman Noris <rognales@gmail.com>
2022-07-28 17:38:59 +0800

    chore: add mail notification for Horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -18,7 +18,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         parent::boot();
 
         // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
+        Horizon::routeMailNotificationsTo(['rognales+fam22@gmail.com', 'sonic21danger+fam22@gmail.com']);
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
         // Horizon::night();
