b6adbb26f0cc9dddfa7332c9cc647fe226aebc01
Zaiman Noris <rognales@gmail.com>
2022-07-28 17:40:54 +0800

    chore: update test

--- tests/Feature/UploadTest.php
+++ tests/Feature/UploadTest.php
@@ -2,31 +2,42 @@
 
 namespace Tests\Feature;
 
-use App\Participant;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Participant;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UploadTest extends TestCase
 {
     use RefreshDatabase;
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_it_should_have_valid_form()
-    {
-        $response = $this->get('/participant/this-is-slug/upload');
-        $response->assertNotFound();
-    }
+    use WithFaker;
 
     public function test_is_should_validate_record()
     {
         $participant = Participant::factory()->create();
 
-        $response = $this->get(route('registration_show', $participant));
+        $response = $this->post(route('registration_upload_store', ['slug' => $participant->slug()]), [
+            'amount' => 112,
+            'reference' => "this is a reference number",
+            'paid_at' => now()->subDay(),
+            'filename' => UploadedFile::fake()->create('document.pdf', $sizeInKilobytes = 2000),
+        ]);
 
-        $response->assertSee($participant->name);
+        $response->assertSessionDoesntHaveErrors();
+    }
+
+    public function test_is_should_handle_invalid_upload()
+    {
+        $participant = Participant::factory()->create();
+
+        $response = $this->post(route('registration_upload_store', ['slug' => $participant->slug()]), [
+            'amount' => 112,
+            'reference' => null,
+            'paid_at' => now()->addDay(),
+            'filename' => UploadedFile::fake()->create('document.pdf', $sizeInKilobytes = 3000),
+        ]);
+
+        $response->assertInvalid(['reference', 'paid_at', 'filename']);
     }
 }
