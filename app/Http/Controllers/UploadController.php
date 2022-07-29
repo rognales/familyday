a49ffb52ae2b138e2b27a638c376bf29744e27ac
Zaiman Noris <rognales@gmail.com>
2022-07-29 12:53:15 +0800

    chore: add more logging statements to upload

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUploadRequest;
-use App\Participant;
 use App\Upload;
+use App\Participant;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\StoreUploadRequest;
 
 class UploadController extends Controller
 {
@@ -16,6 +17,7 @@ class UploadController extends Controller
     public function store(StoreUploadRequest $request, Participant $slug)
     {
         $path = $request->file('filename')->store('uploads');
+        Log::info('Upload::store', [$slug->staff_id, $request->validated(), $path]);
 
         $slug->uploads()->create(array_merge($request->validated(), ['filename' => $path]));
 
@@ -31,6 +33,7 @@ class UploadController extends Controller
     public function show(Upload $upload)
     {
         $extension = pathinfo(storage_path('app/'.$upload->filename), PATHINFO_EXTENSION);
+        Log::info('Upload::show', [$upload->participant->staff_id, $upload]);
 
         return response()->file(storage_path('app/'.$upload->filename), ['Content-Disposition' => "inline; filename={$upload->participant->staff_id}_{$upload->id}.{$extension}"]);
     }
