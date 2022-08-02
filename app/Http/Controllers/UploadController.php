d849534046969fbc15d966ae91c6fcc3b7b63b3a
Zaiman Noris <rognales@gmail.com>
2022-08-03 00:42:00 +0800

    chore: add upload/download from/to s3

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Upload;
+use App\Http\Requests\StoreUploadRequest;
 use App\Participant;
+use App\Upload;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use App\Http\Requests\StoreUploadRequest;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
@@ -17,7 +18,8 @@ class UploadController extends Controller
      */
     public function store(StoreUploadRequest $request, Participant $slug)
     {
-        $path = $request->file('filename')->store('uploads');
+        $path = $request->file('filename')->store('uploads', 'local');
+        $path = $request->file('filename')->store('uploads', 's3');
         Log::info('Upload::store', [$slug->staff_id, $request->validated(), $path]);
 
         $slug->uploads()->create(array_merge($request->validated(), ['filename' => $path]));
@@ -29,13 +31,24 @@ class UploadController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Upload  $upload
-     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse|\Symfony\Component\HttpFoundation\StreamedResponse
      */
     public function show(Request $request, Upload $upload)
     {
         $extension = pathinfo(storage_path('app/'.$upload->filename), PATHINFO_EXTENSION);
+        $filename = "{$upload->participant->staff_id}_{$upload->id}.{$extension}";
+        $headers = ['Content-Disposition' => "inline; filename={$filename}"];
+
         Log::info('Upload::show', [$upload->participant->staff_id, $upload, $request->user()]);
 
-        return response()->file(storage_path('app/'.$upload->filename), ['Content-Disposition' => "inline; filename={$upload->participant->staff_id}_{$upload->id}.{$extension}"]);
+        if (Storage::disk('s3')->exists($upload->filename)) {
+            return Storage::disk('s3')->download($upload->filename, $filename, $headers);
+        }
+
+        if (Storage::disk('local')->exists($upload->filename)) {
+            return Storage::disk('local')->download($upload->filename, $filename, $headers);
+        }
+
+        return response('Files are not found locally or in AWS S3', 404);
     }
 }
