dae0acfdfaaec11bdbfea962f07eabe64438b2d1
Zaiman Noris <rognales@gmail.com>
2022-08-03 19:56:00 +0800

    chore: add missing validation handler on form

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -197,6 +197,9 @@
                                                     <option value="Others"
                                                         @if (old('dependant_relationship' . '.' . $key) == 'Others') selected="selected" @endif>
                                                         Others</option>
+                                                    <option value="Oku"
+                                                        @if (old('dependant_relationship' . '.' . $key) == 'Oku') selected="selected" @endif>
+                                                        Oku</option>
                                                 </select>
                                             </div>
                                             <div class="col-sm-7">
@@ -220,7 +223,7 @@
                                                 <option value="Kids">Children</option>
                                                 <option value="Infant">Infant</option>
                                                 <option value="Others">Others</option>
-                                                <option value="OKU">OKU</option>
+                                                <option value="Oku">OKU</option>
                                             </select>
                                         </div>
                                         <div class="col-sm-7">
